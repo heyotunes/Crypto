f9e3b517216a000de135968893bc6cc6763f9f68
heyotunes <97762771+heyotunes@users.noreply.github.com>
2022-06-27 07:53:53 -0400

    Enter

--- pages/index.js
+++ pages/index.js
@@ -3,19 +3,23 @@ import 'animate.css';
 import styles from  "../styles/Home.module.css";
 
 
-const cryptoEndpoint = 'https://api.abyiss.com/v1/coinbasepro/BTC-USD/currentprice?apiKey=Ql6A^^5)JIo*((*4)BY)kMi81^*40bSXPl^';
 
-export async function getServerSideProps() {
- const res = await fetch(cryptoEndpoint);
-  const data = await res.json();
+
+const cryptoEndpoint = 'https://api.abyiss.com/v1/coinbasepro/BTC-USD/currentprice?apiKey=Ql6A^^5)JIo*((*4)BY)kMi81^*40bSXPl^';
+//fetching the content from abyiss API and storing it in a variable
+
+
+export async function getServerSideProps() { // used async await to get the response while i pass the variable into the function
+ const res = await fetch(cryptoEndpoint);  
+  const data = await res.json();  //i converted the response with JSON into javascript object so that its readable by the browser
   return {
-    props: {
+    props: {   //stored the response in "data" variable 
      data
    }
   }
 }
 
-export default function Home ({ data }) {
+export default function Home ({ data }) {   //passed the response data to the home function to be displayed on webpage by using html and css.
    console.log(data);
 
   return (
@@ -69,12 +73,15 @@ export default function Home ({ data }) {
             <div className={styles.text}>CURRENCY BTC</div>
           <div className={styles.btc}>
             <div className={styles.marketContainer}>
+
               <h2>Market</h2>
               <p>{data.market}</p>
             </div>
             <div className={styles.priceContainer}>
               <h2>Price</h2>
               <p>{data.price}</p>
+              
+
             </div>
            </div>
 
